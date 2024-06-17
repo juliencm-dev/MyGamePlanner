6760d9e8d57ed36a5f31f7ee1167cd156c4fea23
Julien Coulombe <jcoulombemorency@gmail.com>
2024-06-17 09:19:46 +0700

    feat: partial setup of upload things to more easily manage image links

--- next-app/src/components/dashboard/user-information/update-user-avatar.tsx
+++ next-app/src/components/dashboard/user-information/update-user-avatar.tsx
@@ -8,41 +8,51 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { useToast } from "@/components/ui/use-toast";
 import { socket } from "@/components/websocket/socket";
 import { GroupDto } from "@/db/data-access/dto/groups/types";
-import { useRef, useState, useTransition } from "react";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
 import { PulseLoader } from "react-spinners";
 
 export function UpdateUserAvatar({ children, groups }: { children: React.ReactNode; groups: GroupDto[] }) {
   const { toast } = useToast();
-  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
   const imageRef = useRef<HTMLInputElement>(null);
   const [open, setOpen] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(true);
 
-  function handleUpdateAvatar() {
+  function handleIsImgEmpty() {
+    setIsEmpty(!imageRef.current?.files?.length);
+  }
+
+  async function handleUpdateAvatar() {
     const formData = new FormData();
     const imgFile = imageRef.current?.files?.[0];
     formData.append("image", imgFile as File);
 
-    startTransition(async () => {
-      await updateUserAvatarAction(formData).then(res => {
-        if (res.status === 200) {
-          toast({
-            title: "Success",
-            description: res.message,
-            duration: 2500,
-          });
-          groups.forEach(group => {
-            socket.emit("groupUpdate", { target: group.id });
-          });
-        } else {
-          toast({
-            variant: "destructive",
-            title: "Error",
-            description: res.message,
-          });
-        }
-      });
+    setIsLoading(true);
+    await updateUserAvatarAction(formData).then(res => {
+      if (res.status === 200) {
+        toast({
+          title: "Success",
+          description: res.message,
+          duration: 2500,
+        });
+        groups.forEach(group => {
+          socket.emit("groupUpdate", { target: group.id });
+        });
+      } else {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description: res.message,
+        });
+      }
     });
+
+    router.refresh();
+    setIsLoading(false);
     setOpen(false);
+    setIsEmpty(true);
   }
 
   return (
@@ -54,10 +64,10 @@ export function UpdateUserAvatar({ children, groups }: { children: React.ReactNo
           <p className="text-sm font-light">{`Upload a new avatar.`}</p>
           <div className="flex flex-col gap-4 mt-6">
             <Label>Avatar file</Label>
-            <Input name="image" type="file" ref={imageRef} required />
+            <Input name="image" type="file" ref={imageRef} onChange={handleIsImgEmpty} required />
           </div>
-          <Button onClick={handleUpdateAvatar} className="mt-5">
-            {isPending ? <PulseLoader size={4} /> : "Update Avatar"}
+          <Button onClick={handleUpdateAvatar} className="mt-5" disabled={isEmpty}>
+            {isLoading ? <PulseLoader size={4} /> : "Update Avatar"}
           </Button>
         </div>
       </SheetContent>
